b08518741dcce3eaf53e74cb52cd47ed19e801ed
Tejeswara Rao <50865973+tavvatejeswararaotavva@users.noreply.github.com>
2023-01-11 08:50:39 +0530

    Delete spring-security-custom-login-form directory

--- spring-security-custom-login-form/src/main/java/com/playcde/sprigSecurity/demo/config/SecurityWebApplication.java
+++
@@ -1,7 +0,0 @@
-package com.playcde.sprigSecurity.demo.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplication extends AbstractSecurityWebApplicationInitializer {
-
-}
